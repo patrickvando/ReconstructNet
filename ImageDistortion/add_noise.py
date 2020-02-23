c6a75c1387fb375c7a3ea0ac972e8b241d2f318c
Kyle Pham <cpp31@case.edu>
2020-02-23 16:42:15 -0500

    Add comments to add_noise

--- ImageDistortion/add_noise.py
+++ ImageDistortion/add_noise.py
@@ -3,6 +3,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+"""
+Add gaussian noise to the image
+
+:parama		original_image	: the original image (array form)
+:param		Mean  			: mean of gaussian noise
+:param		stddev			: standard deviation
+
+:returns	noisy_image		: the image with noise (array form)
+"""
 def add_gaussian_noise(original_image, mean, stddev):
 	noise = original_image
 	cv.randn(noise, mean, stddev)
@@ -10,6 +19,15 @@ def add_gaussian_noise(original_image, mean, stddev):
 	return noisy_image
 
 
+
+"""
+Add salt and pepper noise to the image
+
+:parama		original_image	: the original image (array form)
+:param		noise_ratio		: the ratio of pixels with noise o
+
+:returns	noisy_image		: the image with noise (array form)
+"""
 def add_salt_and_pepper_noise(original_image, noise_ratio):
 	num_rows, num_columns, num_channels = np.shape(original_image)
 	ret = np.copy(original_image)
@@ -20,11 +38,11 @@ def add_salt_and_pepper_noise(original_image, noise_ratio):
 	return ret
 
 
-img = cv.imread("./beach.jpg")
-noisy_image = add_gaussian_noise(img, 50, 15)
+# img = cv.imread("./beach.jpg")
+# noisy_image = add_gaussian_noise(img, 50, 15)
 
-plt.subplot(121),plt.imshow(img), plt.title('Original')
-plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(noisy_image),plt.title('Blurred')
-plt.xticks([]), plt.yticks([])
-plt.show()
+# plt.subplot(121),plt.imshow(img), plt.title('Original')
+# plt.xticks([]), plt.yticks([])
+# plt.subplot(122),plt.imshow(noisy_image),plt.title('Blurred')
+# plt.xticks([]), plt.yticks([])
+# plt.show()
