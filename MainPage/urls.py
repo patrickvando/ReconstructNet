55431c2ffc217d3dd8cb24ff0449485f9b5e041f
Patrick Do <pvd17@case.edu>
2020-04-18 10:56:31 +0000

    plugged in everything except neural networks

--- MainPage/urls.py
+++ MainPage/urls.py
@@ -5,14 +5,6 @@ from . import views
 urlpatterns = [
         path('', views.index, name='index'),
         path('upload', views.upload, name = 'upload'),
-        path('reset', views.reset, name = 'reset'),
-        path('grayscale_button', views.grayscale_button, name = 'grayscale_button'),
-        path('gaussian_noise_button', views.gaussian_noise_button, name = 'gaussian_noise_button'),
-        path('salt_pepper_noise_button', views.salt_pepper_noise_button, name = 'salt_pepper_noise_button'),
-        path('add_patterns_button', views.add_patterns_button, name = 'add_patterns_button'),
-        path('sharpen_button', views.sharpen_button, name = 'sharpen_button'),
-        path('contrast_button', views.contrast_button, name = 'contrast_button'),
-        path('test', views.testIndex, name = 'T'),
         path('call_alg', views.call_alg, name='call_alg'),
         path('download_image', views.download, name='download_image'),
 ]
--- MainPage/views.py
+++ MainPage/views.py
@@ -5,10 +5,11 @@ from .forms import *
 from PIL import Image
 from .ImageDistortion.add_noise import salt_pepper_noise, gaussian_noise
 from .ImageDistortion.add_patterns import add_random_patterns
+from .ImageDistortion.add_blur import add_gaussian_blur, add_vertical_blur, add_horizontal_blur
 from .ImageDistortion.unsharp_masking import unsharp_mask
 from .ImageDistortion.contrast import increase_contrast 
 import cv2
-import numpy
+import numpy as np
 import shutil
 import os
 import sys
@@ -21,6 +22,7 @@ def call_alg(request):
         if request.GET['wait'] == "true":
             time.sleep(0.5)
         alg_name = request.GET['alg_name']
+        print(alg_name)
         if alg_name == "addSaltAndPepperNoise":
             val = float(request.GET['val'])
             return run_alg(request, salt_pepper_noise, val)
@@ -28,13 +30,17 @@ def call_alg(request):
             val = float(request.GET['val'])
             return run_alg(request, gaussian_noise, val)
         elif alg_name == "addGaussianBlur":
-            pass
+            val = int(request.GET['val'])
+            return run_alg(request, add_gaussian_blur_wrapper, val)
         elif alg_name == "addVerticalBlur":
-            pass
+            val = int(request.GET['val'])
+            return run_alg(request, add_vertical_blur_wrapper, val)
         elif alg_name == "addHorizontalBlur":
-            pass
-        elif alg_name == "inpainting":
-            pass
+            val = int(request.GET['val'])
+            return run_alg(request, add_horizontal_blur_wrapper, val)
+        elif alg_name == "addInpainting":
+            val = int(request.GET['val'])
+            return run_alg(request, add_patterns_wrapper, val)
         elif alg_name == "removeSaltAndPepperNoise":
             pass
         elif alg_name == "removeGaussianNoise":
@@ -47,6 +53,8 @@ def call_alg(request):
             pass
         elif alg_name == "removeInpainting":
             pass
+        elif alg_name == "changeToGrayscale":
+            return run_alg(request, change_to_grayscale_wrapper)
         elif alg_name == "reset":
             user_picture = Picture.objects.get(session_id=request.session.session_key)
             path = user_picture.main_img.path
@@ -77,99 +85,50 @@ def download(request):
     response = FileResponse(open(path, "rb"), as_attachment=True, filename=tail)
     return response
 
-def run_alg(request, alg, val):
+def run_alg(request, alg, val=None):
     user_picture = Picture.objects.get(session_id=request.session.session_key)
     img = cv2.imread(user_picture.edited_img.path)
     img = alg(img, val)
     cv2.imwrite(user_picture.edited_img.path, img)
     return FileResponse(open(user_picture.edited_img.path, 'rb'))
 
+def add_patterns_wrapper(img, d):
+    return add_random_patterns(img, .001, d, d, d)
 
-def grayscale(img_path):
-    img = cv2.imread(img_path)   # reads an image in the BGR format
+def add_gaussian_blur_wrapper(img, d):
+    img = add_gaussian_blur(img, d, 1.0)
+    img = img * 255
+    img = img.astype(np.uint8)
+    return img
+
+def add_vertical_blur_wrapper(img, d):
+    img = add_vertical_blur(img, d)
+    img = img * 255
+    img = img.astype(np.uint8)
+    return img
+
+def add_horizontal_blur_wrapper(img, d):
+    img = add_horizontal_blur(img, d)
+    img = img * 255
+    img = img.astype(np.uint8)
+    return img
+
+def change_to_grayscale_wrapper(img, val):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   # BGR -> RGB
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(img_path,gray)
+    return gray
 
-def contrast_button(request):
-    if request.method == 'GET':
-        user_picture = Picture.objects.get(session_id=request.session.session_key)
-        img = Image.open(user_picture.edited_img.path)
-        img = img.convert("RGB") 
-        img = increase_contrast(img, 5)
-        img.save(user_picture.edited_img.path)
-
-        return FileResponse(open(user_picture.edited_img.path, 'rb'))
-
-def sharpen_button(request):
-    if request.method == 'GET':
-        user_picture = Picture.objects.get(session_id=request.session.session_key)
-        img = Image.open(user_picture.edited_img.path)
-        img = img.convert("RGB") 
-        img = unsharp_mask(img, 7, 5, 5)
-        img.save(user_picture.edited_img.path)
-        return FileResponse(open(user_picture.edited_img.path, 'rb'))
-
-def gaussian_noise_button(request):
-    if request.method == 'GET':
-        sigma = float(request.GET['sigma'])
-        user_picture = Picture.objects.get(session_id=request.session.session_key) 
-        img = cv2.imread(user_picture.edited_img.path)
-        img = gaussian_noise(img, sigma)
-        cv2.imwrite(user_picture.edited_img.path, img)
-        return FileResponse(open(user_picture.edited_img.path, 'rb'))
-
-def salt_pepper_noise_button(request):
-    if request.method == 'GET':
-        d = float(request.GET['d'])
-        print(d)
-        user_picture = Picture.objects.get(session_id=request.session.session_key)
-        img = cv2.imread(user_picture.edited_img.path)
-        img = salt_pepper_noise(img, d)
-        cv2.imwrite(user_picture.edited_img.path, img)
-        return FileResponse(open(user_picture.edited_img.path, 'rb'))
-
-
-def add_patterns_button(request):
-    if request.method == 'GET':
-        print(request.GET)
-        max_ratio = float(request.GET['maxRatio'])
-        max_lines = int(request.GET['maxLines'])
-        max_circles = int(request.GET['maxCircles'])
-        max_ellipses = int(request.GET['maxEllipses'])
-        user_picture = Picture.objects.get(session_id=request.session.session_key)
-        img = cv2.imread(user_picture.edited_img.path)
-        img = add_random_patterns(img, max_ratio, max_lines, max_circles, max_ellipses)
-        cv2.imwrite(user_picture.edited_img.path, img)
-        return FileResponse(open(user_picture.edited_img.path, 'rb'))
-
-def grayscale_button(request):
-    if request.method == 'GET':
-        user_picture = Picture.objects.get(session_id=request.session.session_key)
-        grayscale(user_picture.edited_img.path)
-        return FileResponse(open(user_picture.edited_img.path, 'rb'))
-
-
-def reset(request):
-    if request.method == 'GET':
-        user_picture = Picture.objects.get(session_id=request.session.session_key)
-        path = user_picture.main_img.path
-        save_as_edited_image(path, user_picture)
-        return FileResponse(open(user_picture.edited_img.path, 'rb'))
 
 def index(request): 
     request.session.cycle_key()
     if not request.session.session_key:
         request.session.create()
-    #assign a new database entry to the user if none exists
     if not Picture.objects.filter(session_id = request.session.session_key).exists():
         user_picture = Picture.objects.create(session_id=request.session.session_key)
         path = user_picture.main_img.path
         save_as_edited_image(path, user_picture)
     return render(request, 'MainPage/index.html') 
 
-#user_picture is the database entry
-#takes a path to an image, and saves that image as the "edited image field" in the Picture model
 def save_as_edited_image(img_path, user_picture):
         head, tail = os.path.split(img_path)
         edited_img_directory = head + "/edited_images/"
